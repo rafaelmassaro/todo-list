412c2c1898d29909d15691c2ff9f9807c249ea8e
RAFAEL MASSARO <rafael.massaro@academico.unilasalle.edu.br>
2022-06-11 11:01:28 -0300

    Refatoring complete todo

--- src/components/CustomCheckbox/index.tsx
+++ src/components/CustomCheckbox/index.tsx
@@ -4,16 +4,17 @@ import { CheckboxContainer, HiddenCheckbox, StyledCheckbox, Text } from "./style
 import iconCheck from '../../assets/images/icon-check.svg';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement>{
-    updateItem: () => void;
     children: ReactNode;
+    checkedItem: boolean;
+    completeTodo: () => void;
 }
 
-export function CustomCheckbox({ children, updateItem, ...rest }:Props){
-    const [checked, setChecked] = useState(false);
+export function CustomCheckbox({ children, checkedItem, completeTodo, ...rest }:Props){
+    const [checked, setChecked] = useState(checkedItem);
 
     function handleCheckboxChange(){
         setChecked(!checked);
-        updateItem();
+        completeTodo();
     }
 
     return(
--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -3,31 +3,21 @@ import { CustomCheckbox } from "../CustomCheckbox"
 import { Container } from "./styles"
 
 import iconCross from '../../assets/images/icon-cross.svg';
-import { useContext, useState } from "react";
+import { useContext } from "react";
 
 interface Props{
     item: ListProps;
 }
 
 export function ListItem({item}:Props){
-    const [newItem, setNewItem] = useState<ListProps>(item)
 
-    const { deleteTodo } = useContext(TodoContext);
+    const { deleteTodo, completeTodo } = useContext(TodoContext);
 
-    function updateItem(){
-        const updatedItem = {
-            ...newItem,
-            done: !newItem.done,
-        }
-
-        setNewItem(updatedItem)
-        console.log(updatedItem)
-    }
 
     return(
         <Container>
-            <CustomCheckbox updateItem={updateItem}>
-                {newItem.name}
+            <CustomCheckbox checkedItem={item.done} completeTodo={() => completeTodo(item.id)}>
+                {item.name}
             </CustomCheckbox>
 
             <button
